cd10ba7a5dd0569e879fae3ec968a862269324b7
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2022-01-16 10:19:40 +0000

    Add basic express infrastructure

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.post('/process', function(req, res) {
+    console.log(req.body);
+    
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+export default app;
