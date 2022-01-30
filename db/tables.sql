b58b0d69fcb0cf1566c65b677da25a63136b5e8e
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2022-01-30 18:31:55 +0000

    Refined the details of the postgresql table

--- db/tables.sql
+++ db/tables.sql
@@ -1,14 +1,39 @@
+\c datacollection
+
+DROP TABLE referencepreference;
+
 CREATE TABLE referencepreference (
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL,
     age SMALLINT NOT NULL,
-    area VARCHAR(20)
+    area VARCHAR(20),
+    education VARCHAR(20),
+    acceptance1 VARCHAR(3),
+    reference1 VARCHAR(100),
+    acceptance2 VARCHAR(3),
+    reference2 VARCHAR(100),
+    acceptance3 VARCHAR(3),
+    reference3 VARCHAR(100),
+    acceptance4 VARCHAR(3),
+    reference4 VARCHAR(100),
+    acceptance5 VARCHAR(3),
+    reference5 VARCHAR(100),
+    acceptance6 VARCHAR(3),
+    reference6 VARCHAR(100),
+    acceptance7 VARCHAR(3),
+    reference7 VARCHAR(100),
+    acceptance8 VARCHAR(3),
+    reference8 VARCHAR(100),
+    acceptance9 VARCHAR(3),
+    reference9 VARCHAR(100),
+    acceptance10 VARCHAR(3),
+    reference10 VARCHAR(100)
 );
 GRANT ALL ON referencepreference TO yixuan;
 GRANT ALL ON referencepreference_id_seq TO yixuan;
 
-DROP TABLE referencepreference;
 
-INSERT INTO referencepreference(firstName, lastName, email, age) VALUES ('Yixuan', 'Wu', 'yixuan@wu.ch', '25');
+
+INSERT INTO referencepreference(firstName, lastName, email, age, education, acceptance1, reference1, acceptance2, reference2, acceptance3, reference3, acceptance4, reference4, acceptance5, reference5, acceptance6, reference6, acceptance7, reference7, acceptance8, reference8, acceptance9, reference9, acceptance10, reference10) VALUES ('Yixuan', 'Wu', 'yixuan@wu.ch', '25', 'Master', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26');
--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.post('/process', function (req, res) {
-    const insertQuery = 'INSERT INTO referencepreference(firstName, lastName, email, age, area) VALUES ($1, $2, $3, $4, $5);'
-    const values = [req.body.firstName, req.body.lastName, req.body.email, req.body.age, req.body.area];
+    const insertQuery = 'INSERT INTO referencepreference(firstName, lastName, email, age, area, education, acceptance1, reference1, acceptance2, reference2, acceptance3, reference3, acceptance4, reference4, acceptance5, reference5, acceptance6, reference6, acceptance7, reference7, acceptance8, reference8, acceptance9, reference9, acceptance10, reference10) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26);'
+    const values = [req.body.firstName, req.body.lastName, req.body.email, req.body.age, req.body.area, req.body.education, req.body.acceptance1, req.body.reference1, req.body.acceptance2, req.body.reference2, req.body.acceptance3, req.body.reference3, req.body.acceptance4, req.body.reference4, req.body.acceptance5, req.body.reference5, req.body.acceptance6, req.body.reference6, req.body.acceptance7, req.body.reference7, req.body.acceptance8, req.body.reference8, req.body.acceptance9, req.body.reference9, req.body.acceptance10, req.body.reference10];
 
     pool.query(insertQuery, values).then(insertRes => {
       res.redirect('/success.html');
